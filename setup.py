4d95a2f4ac57137cf3c7cb5166b150fba29ea14f
Camille Potard <cpotard@gmail.com>
2016-06-03 09:54:32 +0200

    added dependencies

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
         'Programming Language :: Python :: 3.3',
     ],
     zip_safe=False,
-    install_requires=['argparse', 'requests', 'python-dateutil'],
+    install_requires=['argparse', 'requests', 'python-dateutil', 'beautifulsoup4', 'facepy'],
     entry_points="""
         [console_scripts]
             vecc = vecc.vecc:main"""
